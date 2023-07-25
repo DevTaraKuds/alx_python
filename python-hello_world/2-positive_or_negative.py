e4f50e0f92f156a8af74a1b3ae9a0951265aef62
DevTaraKuds <omotarakuds@gmail.com>
2023-07-25 12:35:53 +0100

    determining negative and positive values

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 
-if number > 0:
+if (number > 0):
     print(number, " is positive")
-elif number == 0:
+elif (number == 0):
     print(number, " is zero") 
-elif number < 0:
+else:
     print(number, " is negative", end="\n")
 
