ead56d44db9b1edae76f0f621b3577ec723526d1
DevTaraKuds <omotarakuds@gmail.com>
2023-07-29 17:44:58 +0100

    update

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -20,9 +20,9 @@ last_digit = abs(number) % 10
 
 # Check the last digit and print the output accordingly
 if last_digit > 5:
-    print("The string Last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit == 0:
-    print("The string Last digit of", number, "is", last_digit, "and is 0")
+    print("Last digit of", number, "is", last_digit, "and is 0")
 else:
-    print("The string Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
 
