9000e3e7515bd034b9e6d1f3c5a3b783e7c24106
DevTaraKuds <omotarakuds@gmail.com>
2023-07-29 17:31:44 +0100

    hexadecimal values displayed

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,8 @@
-for i in range(0,98):
+# for i in range(0,98):
     # print(str({}).format((hex(i))))
-    print(i, "=", str(hex(i)))
+    # print(i, "=", str(hex(i)))
+
+for number in range(99):
+    # print("{:<3} = {:02X}".format(number, number))
+    print("{:<3} = {}".format(number, hex(number)))
+
