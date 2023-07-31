65fa9a5a2e7bc1469df2100f4f400886780b4629
DevTaraKuds <omotarakuds@gmail.com>
2023-07-31 02:35:23 +0100

    new

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,27 +2,29 @@
 import random
 number = random.randint(-10000, 10000)
 # # YOUR CODE HERE
-# number=str(number)
-# lastDigit = abs(int(number[-1]))
+number=str(number)
+lastDigit = int(number[-1])
 
-# if lastDigit > 5:
-#     valueText = "and is greater than 5"
-# elif (lastDigit < 6) and (lastDigit != 0):
-#     valueText = "and is less than 6 and not 0"
-# else:
-#     valueText = "and is 0"
+if lastDigit > 5:
+    valueText = "and is greater than 5"
+elif lastDigit == 0 :
+    valueText = "and is 0"
+else:
+    valueText = "and is less than 6 and not 0"
 
 
-# print("Last digit of", number, "is", lastDigit, valueText, end="\n")
+
+print("Last digit of", number, "is", lastDigit, valueText, end="\n")
+
 
 # Get the last digit of the number
-last_digit = number % 10
+# last_digit = number % 10
 
-# Check the last digit and print the output accordingly
-if last_digit > 5:
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
-elif last_digit == 0:
-    print("Last digit of", number, "is", last_digit, "and is 0")
-else:
-    print("Last digit of", number, "is", -last_digit, "and is less than 6 and not 0")
+# # Check the last digit and print the output accordingly
+# if last_digit > 5:
+#     print("Last digit of", number, "is", last_digit, "and is greater than 5")
+# elif last_digit == 0:
+#     print("Last digit of", number, "is", last_digit, "and is 0")
+# else:
+#     print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
 
