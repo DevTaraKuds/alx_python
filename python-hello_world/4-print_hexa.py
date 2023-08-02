948843c48844399d7764ca25e4fca4c9b4c9cf67
DevTaraKuds <omotarakuds@gmail.com>
2023-08-02 07:03:02 +0100

    update

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,8 +1,6 @@
-# for i in range(0,98):
-    # print(str({}).format((hex(i))))
-    # print(i, "=", str(hex(i)))
+#!/usr/bin/python3
 
 for number in range(99):
     # print("{:<3} = {:02X}".format(number, number))
-    print("{} = {}".format(number, str(hex(number))))
+    print("{} = {}".format(number, hex(number)))
 
