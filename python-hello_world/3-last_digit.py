987c242969d40e5825f6a1d4184b248f4a61ab92
DevTaraKuds <omotarakuds@gmail.com>
2023-08-02 06:50:04 +0100

    corrected checker errors

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,14 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 # # YOUR CODE HERE
 number=str(number)
+
 lastDigit = int(number[-1])
+if int(number) < 0:
+    lastDigit = -lastDigit
 
 if lastDigit > 5:
     valueText = "and is greater than 5"
 elif lastDigit == 0 :
     valueText = "and is 0"
-elif lastDigit < 0:
-    lastDigit = -lastDigit
+else:
     valueText = "and is less than 6 and not 0"
 
 
